337ba8e1517fe0be351fa8868ee58bee654ff974
zhuangzhuang <zhuangzhuang1988@outlook.com>
2016-12-30 13:27:04 +0800

    finish step

--- step.ts
+++ step.ts
@@ -90,13 +90,45 @@ var step = function(state){
     var data = state.data;
     var done = state.done;
     if(done) 
-        return;    
-    data = data.func.apply(null, data.args);
-    if(data.tag === 'thunk'){
-        state.data = data;        
-    }else{
+        return;   
+    if(data.tag === 'value') {
         state.data = data.val;
         state.done = true;
-    }    
+        return;
+    }
+    data = data.func.apply(null, data.args);
+    state.data = data;    
 }
 
+// http://nathansuniversity.com/step3.html
+var evalTwo = function (expr0, expr1, env) {
+    var state1 = stepStart(expr0, env);
+    var state2 = stepStart(expr1, env);
+    while(!state1.done || !state2.done) {
+        if(!state1.done) {
+            step(state1);
+        }
+        if(!state2.done){
+            step(state2);
+        }
+    }    
+};
+
+// http://nathansuniversity.com/step4.html
+var evalDiv = function (top, bottom, env, cont, xcont) {
+    // Here's the code for addition
+    // to help you get going.
+    return thunk(
+        evalExpr, top, env,
+        function(v1) {
+            return thunk(
+                evalExpr, bottom, env,
+                function(v2) {
+                    if(v2 !== 0){
+                        return thunk(cont, v1 / v2);
+                    }else{
+                        return thunk(xcont, 'EXCEPTION!');
+                    }
+            }, xcont);
+    }, xcont);
+};
